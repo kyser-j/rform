525a4ba0068e55012d1fc3b5deb03972755347f3
Josh Kyser <kyser.j.dev@gmail.com>
2025-07-17 06:29:57 -0500

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import PageRenderer from "./components/PageRenderer";
+import testForm from "./lib/test-files/testForm";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <section>
+      {testForm.pages.map(page => 
+
+      <PageRenderer key={page.id} page={page} />
+      )}
+    </section>
   );
 }
 
